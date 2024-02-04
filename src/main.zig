2f496fe2412f028fd1e8f7d9b58220cdbe6f108b
bluesillybeard <bluesillybeard2@gmail.com>
2024-02-04 16:15:24 -0700

    Initial rendering test

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,17 @@ const std = @import("std");
 const zengine = @import("zengine");
 const zrender = @import("zrender");
 
-pub fn main() void {
-    std.debug.print("\n\nHello!!\n\n", .{});
+pub fn main() !void {
+    var allocatorObj = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = allocatorObj.allocator();
+
+    const ZEngine = zengine.ZEngine(.{
+        .globalSystems = &[_]type{zrender.ZRenderSystem},
+        .localSystems = &[_]type{},
+    });
+    var engine = try ZEngine.init(allocator);
+    var zrenderSystem = engine.registries.globalRegistry.getRegister(zrender.ZRenderSystem).?;
+    try zrenderSystem.initZRender(&engine.registries);
+    zrenderSystem.run();
+    defer engine.deinit();
 }
