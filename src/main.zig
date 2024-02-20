4cbc3215952cfc95f6021c9b4b0866c4ea76a0d8
bluesillybeard <bluesillybeard2@gmail.com>
2024-02-19 19:46:55 -0700

    interpolate between updates

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,8 @@ pub fn main() !void {
 
 pub const ExampleComponent = extern struct {
     rotation: f32,
+    // rotation in the previous update
+    lastRotation: f32,
 };
 
 pub const ExampleSystem = extern struct {
@@ -33,7 +35,7 @@ pub const ExampleSystem = extern struct {
     pub fn init(staticAllocator: std.mem.Allocator, heapAllocator: std.mem.Allocator) @This() {
         _ = heapAllocator;
         _ = staticAllocator;
-        return .{ .cameraRotation = 0 };
+        return .{ .cameraRotation = 0, .lastCameraRotation = 0 };
     }
 
     pub fn systemInitGlobal(this: *@This(), registries: *zengine.RegistrySet) !void {
@@ -50,58 +52,74 @@ pub const ExampleSystem = extern struct {
             .texture = try renderSystem.loadTexture(@embedFile("parrot.png")),
             .transform = zrender.Mat4.identity,
         });
-        registries.globalEcsRegistry.add(entity, ExampleComponent{ .rotation = 0 });
+        registries.globalEcsRegistry.add(entity, ExampleComponent{ .rotation = 0, .lastRotation = 0 });
         renderSystem.onUpdate.sink().connect(&update);
+        renderSystem.onFrame.sink().connect(&frame);
         renderSystem.onType.sink().connect(&onType);
         renderSystem.onKeyDown.sink().connect(&onKeyDown);
+        renderSystem.onKeyUp.sink().connect(&onKeyUp);
     }
 
     fn onKeyDown(args: zrender.OnKeyDownEventArgs) void {
         std.debug.print("Key {} down\n", .{args.key});
     }
 
+    fn onKeyUp(args: zrender.OnKeyUpEventArgs) void {
+        std.debug.print("Key {} up\n", .{args.key});
+    }
+
     fn onType(args: zrender.OnTypeEventArgs) void {
-        var buffer = [5]u8{ 0, 0, 0, 0, 0 };
+        // 4 bytes for the longest codepoint, one more for null terminator
+        var buffer = [4:0]u8{ 0, 0, 0, 0 };
         // TODO: make sure the character fits in a u21
         _ = std.unicode.utf8Encode(@intCast(args.character), &buffer) catch std.debug.print("Warn: Invalid unicode point: {}", .{args.character});
         std.debug.print("Typed, {s}\n", .{buffer});
     }
 
     fn update(args: zrender.OnUpdateEventArgs) void {
+        // TODO: incorperate delta into calculations
         var ecs = args.registries.globalEcsRegistry;
-        var self = args.registries.globalRegistry.getRegister(ExampleSystem).?;
-        self.cameraRotation += 0.01;
+        var this = args.registries.globalRegistry.getRegister(ExampleSystem).?;
+        const deltaSeconds = @as(f32, @floatFromInt(args.delta)) / std.time.us_per_s;
+        this.lastCameraRotation = this.cameraRotation;
+        this.cameraRotation += std.math.pi / 8.0 * deltaSeconds;
         var view = ecs.view(.{ ExampleComponent, zrender.RenderComponent }, .{});
         var iter = view.entityIterator();
         while (iter.next()) |entity| {
             const exampleComponent = view.get(ExampleComponent, entity);
+            exampleComponent.lastRotation = exampleComponent.rotation;
+            exampleComponent.rotation += std.math.pi * deltaSeconds;
+        }
+    }
+
+    fn frame(args: zrender.OnFrameEventArgs) void {
+        var ecs = args.registries.globalEcsRegistry;
+        const this = args.registries.globalRegistry.getRegister(ExampleSystem).?;
+        const renderSystem = args.registries.globalRegistry.getRegister(zrender.ZRenderSystem).?;
+        var view = ecs.view(.{ ExampleComponent, zrender.RenderComponent }, .{});
+        var iter = view.entityIterator();
+        // From 0 to 1 (and often >1), how far into the current update are we?
+        // 0 -> time == updateTime
+        // 1 -> time == updateTime + updateDelta
+        // g
+        const t = @as(f32, @floatFromInt(args.time - renderSystem.updateTime)) / @as(f32, @floatFromInt(renderSystem.updateDelta));
+        while (iter.next()) |entity| {
+            // TODO: lots of optimization potential here
+            const exampleComponent = view.get(ExampleComponent, entity);
             const renderComponent = view.get(zrender.RenderComponent, entity);
-            exampleComponent.rotation += 0.3;
+            var lastTransform = zlm.Mat4.identity;
             var transform = zlm.Mat4.identity;
             // Object transformation
             transform = transform.mul(zlm.Mat4.createAngleAxis(zlm.Vec3.unitZ, exampleComponent.rotation));
-            // camera transformation
-            transform = transform.mul(zlm.Mat4.createLookAt(zlm.Vec3{ .x = @cos(self.cameraRotation) * 5, .y = 0, .z = @sin(self.cameraRotation) * 5 }, zlm.Vec3.zero, zlm.Vec3.unitY));
-            transform = transform.mul(zlm.Mat4.createPerspective(zlm.toRadians(80.0), 1, 0.0001, 10000));
+            lastTransform = lastTransform.mul(zlm.Mat4.createAngleAxis(zlm.Vec3.unitZ, exampleComponent.lastRotation));
 
-            renderComponent.transform = zrender.Mat4{
-                .m00 = transform.fields[0][0],
-                .m01 = transform.fields[1][0],
-                .m02 = transform.fields[2][0],
-                .m03 = transform.fields[3][0],
-                .m10 = transform.fields[0][1],
-                .m11 = transform.fields[1][1],
-                .m12 = transform.fields[2][1],
-                .m13 = transform.fields[3][1],
-                .m20 = transform.fields[0][2],
-                .m21 = transform.fields[1][2],
-                .m22 = transform.fields[2][2],
-                .m23 = transform.fields[3][2],
-                .m30 = transform.fields[0][3],
-                .m31 = transform.fields[1][3],
-                .m32 = transform.fields[2][3],
-                .m33 = transform.fields[3][3],
-            };
+            // camera transformation
+            transform = transform.mul(zlm.Mat4.createLookAt(zlm.Vec3{ .x = @cos(this.cameraRotation) * 5, .y = 0, .z = @sin(this.cameraRotation) * 5 }, zlm.Vec3.zero, zlm.Vec3.unitY));
+            transform = transform.mul(zlm.Mat4.createPerspective(zlm.toRadians(80.0), 1, 0.0001, 10000));
+            lastTransform = lastTransform.mul(zlm.Mat4.createLookAt(zlm.Vec3{ .x = @cos(this.lastCameraRotation) * 5, .y = 0, .z = @sin(this.lastCameraRotation) * 5 }, zlm.Vec3.zero, zlm.Vec3.unitY));
+            lastTransform = lastTransform.mul(zlm.Mat4.createPerspective(zlm.toRadians(80.0), 1, 0.0001, 10000));
+
+            renderComponent.transform = zlmToZrenderMat4(matrixLerp(lastTransform, transform, t));
         }
     }
 
@@ -113,5 +131,54 @@ pub const ExampleSystem = extern struct {
     pub fn deinit(this: *@This()) void {
         _ = this;
     }
+
+    fn zlmToZrenderMat4(matrix: zlm.Mat4) zrender.Mat4 {
+        return zrender.Mat4{
+            .m00 = matrix.fields[0][0],
+            .m01 = matrix.fields[1][0],
+            .m02 = matrix.fields[2][0],
+            .m03 = matrix.fields[3][0],
+            .m10 = matrix.fields[0][1],
+            .m11 = matrix.fields[1][1],
+            .m12 = matrix.fields[2][1],
+            .m13 = matrix.fields[3][1],
+            .m20 = matrix.fields[0][2],
+            .m21 = matrix.fields[1][2],
+            .m22 = matrix.fields[2][2],
+            .m23 = matrix.fields[3][2],
+            .m30 = matrix.fields[0][3],
+            .m31 = matrix.fields[1][3],
+            .m32 = matrix.fields[2][3],
+            .m33 = matrix.fields[3][3],
+        };
+    }
+
+    fn zrenderToZlmMat4(m: zrender.Mat4) zlm.Mat4 {
+        return zrender.Mat4{ .fields = [_][4]f32{
+            [_]f32{ m.m00, m.m10, m.m20, m.m30 },
+            [_]f32{ m.m01, m.m11, m.m21, m.m31 },
+            [_]f32{ m.m02, m.m12, m.m22, m.m32 },
+            [_]f32{ m.m03, m.m13, m.m23, m.m33 },
+        } };
+    }
+
+    fn matrixLerp(m0: zlm.Mat4, m1: zlm.Mat4, t: f32) zlm.Mat4 {
+        // TODO: vectorize?
+        return zlm.Mat4{ .fields = [_][4]f32{
+            [4]f32{ fLerp(m0.fields[0][0], m1.fields[0][0], t), fLerp(m0.fields[0][1], m1.fields[0][1], t), fLerp(m0.fields[0][2], m1.fields[0][2], t), fLerp(m0.fields[0][3], m1.fields[0][3], t) },
+            [4]f32{ fLerp(m0.fields[1][0], m1.fields[1][0], t), fLerp(m0.fields[1][1], m1.fields[1][1], t), fLerp(m0.fields[1][2], m1.fields[1][2], t), fLerp(m0.fields[1][3], m1.fields[1][3], t) },
+            [4]f32{ fLerp(m0.fields[2][0], m1.fields[2][0], t), fLerp(m0.fields[2][1], m1.fields[2][1], t), fLerp(m0.fields[2][2], m1.fields[2][2], t), fLerp(m0.fields[2][3], m1.fields[2][3], t) },
+            [4]f32{ fLerp(m0.fields[3][0], m1.fields[3][0], t), fLerp(m0.fields[3][1], m1.fields[3][1], t), fLerp(m0.fields[3][2], m1.fields[3][2], t), fLerp(m0.fields[3][3], m1.fields[3][3], t) },
+        } };
+    }
+    // Std's lerp function has asserts and other stuff.
+    // So, a "fast and loose" version is used, which does not already exist in std for some reason.
+    // t is not bounds checked because during lag t may be greater than 1.
+    // The formula works just fine without it, honestly I don't even know why std would bother doing such a pointless check, rendering it useless for my use case.
+    inline fn fLerp(a: f32, b: f32, t: f32) f32 {
+        return @mulAdd(f32, b - a, t, a);
+    }
     cameraRotation: f32,
+    // camera rotation in the last update
+    lastCameraRotation: f32,
 };
